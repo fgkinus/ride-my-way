927402bea66d80559a8bcde6e143de4e0e3254da
fgkinus <fgkinus@gmail.com>
2018-06-20 15:17:43 +0300

    Added simple trip database

--- API/V1/rides/__init__.py
+++ API/V1/rides/__init__.py
@@ -1,15 +1,15 @@
 """
-Initilize the rides Package and instantiate the API
+Initialize the rides Package and instantiate the API
 """
 from flask import Blueprint, jsonify
 from flask_restplus import Api
-
 
 rides_blueprint = Blueprint('rides', __name__)
 
 # Set up  the API and init the blueprint
 api = Api(version='1.0', title='Rides EndPoint', description='Modify and interact with the Rides endpoints')
 api.init_app(rides_blueprint)
+
 
 # Set the default route
 @rides_blueprint.route('/')
