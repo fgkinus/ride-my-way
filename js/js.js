ae62422c172e6e110b4e017b52c616bdae81b5e3
fgkinus <fgkinus@gmail.com>
2018-06-27 14:22:29 +0300

    editted the js script reference to point to right file

--- js/js.js
+++ js/js.js
@@ -2,9 +2,9 @@
 
 
 function autoResizeDiv(element) {
-    // a function to resize elements and te iframe
+    // a function to resize elements and the iframe
     window.onload = function () {
-        document.getElementById(element).style.height= window.innerHeight+'px';
+        document.getElementById(element).style.height = window.innerHeight + 'px';
     }
     window.onresize = autoResizeDiv(element);
     return this;
@@ -14,34 +14,34 @@ function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("openNav").style.display = 'none';
 }
+
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
 }
 
 function open_modal() {
     var modal = document.getElementById('add_trip');
-    modal.style.display  = 'block';
+    modal.style.display = 'block';
 }
 
-function close_IFrame()
-{
+function close_IFrame() {
     var modal = document.getElementById('add_trip');
     modal.style.display = 'none';
 }
 
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-    for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
 
 
