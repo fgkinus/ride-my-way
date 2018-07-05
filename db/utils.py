7dd3ae622e4271882ff6c0a42acfb8e2dd84ad59
fgkinus <fgkinus@gmail.com>
2018-07-05 21:10:01 +0300

    added database object

--- db/utils.py
+++ db/utils.py
@@ -2,59 +2,158 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2.extras import RealDictCursor
 
-
-def close(connection):
-    connection.close()
-    return connection
+from db import queries
 
 
-def query_db(conn, query, args):
-    """
-    a custom function to run any valid SQL query provided a connection object is provided.
-    If a transaction fails , its rolled back and cursors are not blocked
-    :param conn:
-    :param query:
-    :param args:
-    :return result:
-    """
-    with conn:
-        with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute(query, args)
-            result = cur.fetchall()
-            return result
+#
+#
+# def close(connection):
+#     connection.close()
+#     return connection
+#
+#
+# def query_db(conn, query, args):
+#     """
+#     a custom function to run any valid SQL query provided a connection object is provided.
+#     If a transaction fails , its rolled back and cursors are not blocked
+#     :param conn:
+#     :param query:
+#     :param args:
+#     :return result:
+#     """
+#     with conn:
+#         with conn.cursor(cursor_factory=RealDictCursor) as cur:
+#             cur.execute(query, args)
+#             result = cur.fetchall()
+#             return result
+#
+#
+# def add_table(conn, query):
+#     """
+#     a custom function to run any valid SQL query provided a connection object is provided.
+#     If a transaction fails , its rolled back and cursors are not blocked
+#     :param conn:
+#     :param query:
+#     :return:
+#     """
+#     with conn:
+#         with conn.cursor() as cur:
+#             cur.execute(query)
+#
+#
+# def create_tables(conn, queries):
+#     for query in queries:
+#         add_table(conn=conn, query=query)
+#
+#
+# def check_db_exists(conn, db_name='rmw'):
+#     """Verify that db exists"""
+#     query = """select exists(
+#              SELECT datname FROM pg_catalog.pg_database WHERE datname=%s
+#             );
+#             """
+#     return query_db(conn, query, (db_name,))
+#
+#
+# def create_db(conn, dbname):
+#     query = """CREATE DATABASE {} ENCODING ='utf8'""".format(dbname)
+#     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+#     with conn:
+#         with conn.cursor() as cur:
+#             cur.execute(query)
+#     conn.close()
 
 
-def add_table(conn, query):
-    """
-    a custom function to run any valid SQL query provided a connection object is provided.
-    If a transaction fails , its rolled back and cursors are not blocked
-    :param conn:
-    :param query:
-    :return:
-    """
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute(query)
+class Database(object):
+
+    def __init__(self, username, password, db_name):
+        self.username = username
+        self.password = password
+        self.db_name = db_name
+        self.conn = None
+
+    @staticmethod
+    def connect_db(db_name, username, password):
+        try:
+            conn = psycopg2.connect(database=db_name, user=username, password=password)
+            return conn
+        except Exception:
+            raise psycopg2.DatabaseError("could not connect to database")
+
+    def create_db(self, dbname, username, password):
+        query = """CREATE DATABASE {} ENCODING ='utf8'""".format(dbname)
+        conn = self.connect_db('postgres', username, password)
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        self.query_db_no_result(self.conn, query, ())
+        conn.close()
+
+    def create_tables(self, conn, create_queries=queries.tables_list):
+        for query in create_queries:
+            self.query_db_no_result(conn=conn, query=query, args=())
+
+    @staticmethod
+    def query_db(conn, query, args):
+        """
+        a custom function to run any valid SQL query provided a connection object is provided.
+        If a transaction fails , its rolled back and cursors are not blocked
+        :param conn:
+        :param query:
+        :param args:
+        :return result:
+        """
+        with conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(query, args)
+                result = cur.fetchall()
+                return result
+
+    @staticmethod
+    def query_db_no_result(conn, query, args):
+        """
+        a custom function to run any valid SQL query provided a connection object is provided.
+        If a transaction fails , its rolled back and cursors are not blocked
+        :param conn:
+        :param query:
+        :param args:
+        :return :
+        """
+        with conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(query, args)
+
+    def check_db_exists(self, conn, db_name='rmw'):
+        """Verify that db exists"""
+        query = """select exists(
+                 SELECT datname FROM pg_catalog.pg_database WHERE datname=%s
+                );
+                """
+        return self.query_db(conn, query, (db_name,))
+
+    @staticmethod
+    def close_conn(conn):
+        conn.close()
+        return conn
+
+    def get_connection(self):
+        if isinstance(self.conn, psycopg2.extensions.connection):
+            return self.conn
+        else:
+            raise ConnectionError("connection not established.please initialise the DB first")
+
+    def init_db(self):
+        """initialise database"""
+        # create db connecton to root db
+        conn = self.connect_db(db_name='postgres', username=self.username, password=self.password)
+        # verify db exists
+        if self.check_db_exists(conn=conn, db_name=self.db_name)[0]['exists']:
+            # set connection
+            self.conn = self.connect_db(db_name=self.db_name, username=self.username, password=self.password)
+            return self
+        else:
+            # create db and initialise connection
+            self.create_db(self.db_name, password=self.password, username=self.username)
+            self.conn = self.connect_db(db_name=self.db_name, username=self.password, password=self.password)
+            self.create_tables(conn=self.conn)
+            return self
 
 
-def create_tables(conn, queries):
-    for query in queries:
-        add_table(conn=conn, query=query)
-
-
-def check_db_exists(conn, db_name='rmw'):
-    """Verify that db exists"""
-    query = """select exists(
-             SELECT datname FROM pg_catalog.pg_database WHERE datname=%s
-            );
-            """
-    return query_db(conn, query, (db_name,))
-
-
-def create_db(conn, dbname):
-    query = """CREATE DATABASE {} ENCODING ='utf8'""".format(dbname)
-    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute(query)
-    conn.close()
