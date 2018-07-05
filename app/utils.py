dde42439f46ade2cfff7fa17827847f651548bf8
fgkinus <fgkinus@gmail.com>
2018-07-05 21:10:30 +0300

    removed daabase initialisation and connection functions

--- app/utils.py
+++ app/utils.py
@@ -3,42 +3,6 @@ from flask import Flask
 import os
 
 from config import APP_CONFIG, basedir
-import psycopg2
-from psycopg2.extras import RealDictCursor
-
-from db.queries import tables_list
-from db.utils import check_db_exists, create_db, create_tables
-
-
-def connect_db(db_name, user, password):
-    try:
-        conn = psycopg2.connect(database=db_name, user=user, password=password)
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        return conn, cursor
-    except Exception:
-        raise psycopg2.DatabaseError
-
-
-def init_db(db_name, username='postgres', password=''):
-    try:
-        conn = psycopg2.connect(database='postgres', user=username, password=password)
-    except psycopg2.DatabaseError:
-        raise psycopg2.DatabaseError("could not initiate database connection")
-    if check_db_exists(conn=conn, db_name=db_name)[0]['exists']:
-        pass
-    else:
-        create_db(conn, db_name)
-        conn = connect_db(db_name=db_name, user='postgres', password=os.getenv('DATABASE_PASSWORD'))
-        create_tables(conn[0], tables_list)
-
-
-# def register_blueprints(app):
-#     """Register the different blueprints with the application"""
-#
-#     from app.users import users_blueprint
-#     from app.rides import rides_blueprint
-#     app.register_blueprint(users_blueprint, url_prefix='/api/v1/auth')
-#     app.register_blueprint(rides_blueprint, url_prefix='/api/v1')
 
 
 def register_namespace(root):
