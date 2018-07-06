d2aa2d3d285d2b171ae46f243bd2a032ebed557f
fgkinus <fgkinus@gmail.com>
2018-07-06 06:02:55 +0300

    moved the application factory and namespace registration to app __init__

--- app/utils.py
+++ app/utils.py
@@ -1,26 +1,7 @@
-from __future__ import absolute_import
-from flask import Flask
-import os
+# initialise the database object
+import run
+from db.utils import Database
 
-from config import APP_CONFIG, basedir
-
-
-def register_namespace(root):
-    """
-    add namespaces to endpoints
-    :param root:
-    :return:
-    """
-    from app.rides.Resources import api as rides_ns
-    from app.users.Resources import api as users_ns
-    root.add_namespace(rides_ns, path='/api/v2')
-    root.add_namespace(users_ns, path='/api/v2/auth')
-
-
-def create_app(config_name):
-    """The application factory function"""
-
-    app = Flask(__name__)
-    app.config.from_object(APP_CONFIG[config_name])
-    app.config.from_pyfile(os.path.join(basedir, 'config.py'))
-    return app
+app = run.app
+Database = Database(username=app.config['DATABASE_USER'], password=app.config['DATABASE_PASSWORD'],
+                    db_name=app.config['DATABASE_NAME']).init_db()
