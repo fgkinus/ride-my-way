4b84db9e301871dc75cf9e260a127555655a3547
fgkinus <fgkinus@gmail.com>
2018-07-06 08:04:41 +0300

    added supprt for heroku url to methods

--- db/utils.py
+++ db/utils.py
@@ -5,80 +5,29 @@ from psycopg2.extras import RealDictCursor
 from db import queries
 
 
-#
-#
-# def close(connection):
-#     connection.close()
-#     return connection
-#
-#
-# def query_db(conn, query, args):
-#     """
-#     a custom function to run any valid SQL query provided a connection object is provided.
-#     If a transaction fails , its rolled back and cursors are not blocked
-#     :param conn:
-#     :param query:
-#     :param args:
-#     :return result:
-#     """
-#     with conn:
-#         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-#             cur.execute(query, args)
-#             result = cur.fetchall()
-#             return result
-#
-#
-# def add_table(conn, query):
-#     """
-#     a custom function to run any valid SQL query provided a connection object is provided.
-#     If a transaction fails , its rolled back and cursors are not blocked
-#     :param conn:
-#     :param query:
-#     :return:
-#     """
-#     with conn:
-#         with conn.cursor() as cur:
-#             cur.execute(query)
-#
-#
-# def create_tables(conn, queries):
-#     for query in queries:
-#         add_table(conn=conn, query=query)
-#
-#
-# def check_db_exists(conn, db_name='rmw'):
-#     """Verify that db exists"""
-#     query = """select exists(
-#              SELECT datname FROM pg_catalog.pg_database WHERE datname=%s
-#             );
-#             """
-#     return query_db(conn, query, (db_name,))
-#
-#
-# def create_db(conn, dbname):
-#     query = """CREATE DATABASE {} ENCODING ='utf8'""".format(dbname)
-#     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-#     with conn:
-#         with conn.cursor() as cur:
-#             cur.execute(query)
-#     conn.close()
-
-
 class Database(object):
 
-    def __init__(self, username, password, db_name):
+    def __init__(self, username, password, db_name, url=None):
         self.username = username
         self.password = password
         self.db_name = db_name
+        self.url = url
         self.conn = None
 
     @staticmethod
-    def connect_db(db_name, username, password):
-        try:
-            conn = psycopg2.connect(database=db_name, user=username, password=password)
-            return conn
-        except Exception:
-            raise psycopg2.DatabaseError("could not connect to database")
+    def connect_db(db_name, username, password, url=None):
+        if url is None:
+            try:
+                conn = psycopg2.connect(database=db_name, user=username, password=password)
+                return conn
+            except Exception:
+                raise psycopg2.DatabaseError("could not connect to database")
+        else:
+            try:
+                conn = psycopg2.connect(url, sslmode='require')
+                return conn
+            except Exception:
+                raise psycopg2.DatabaseError("could not connect to database")
 
     def get_connection(self):
         if isinstance(self.conn, psycopg2.extensions.connection):
@@ -86,14 +35,21 @@ class Database(object):
         else:
             raise ConnectionError("connection not established.please initialise the DB first")
 
-    def create_db(self, dbname, username, password):
-        query = """CREATE DATABASE {} ENCODING ='utf8'""".format(dbname)
-        conn = self.connect_db('postgres', username, password)
-        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        self.query_db_no_result(query, ())
-        conn.close()
+    def create_db(self, dbname, username, password, url=None):
+        if url is None:
+            query = """CREATE DATABASE {} ENCODING ='utf8'""".format(dbname)
+            conn = self.connect_db('postgres', username, password)
+            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            self.query_db_no_result(query, ())
+            conn.close()
+        else:
+            query = """CREATE DATABASE {} ENCODING ='utf8'""".format(dbname)
+            conn = self.connect_db('postgres', username, password, url=self.url)
+            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            self.query_db_no_result(query, ())
+            conn.close()
 
-    def create_tables(self, conn, create_queries=queries.tables_list):
+    def create_tables(self, create_queries=queries.tables_list):
         for query in create_queries:
             self.query_db_no_result(query=query, args=())
 
@@ -144,15 +100,17 @@ class Database(object):
     def init_db(self):
         """initialise database"""
         # create db connecton to root db
-        self.conn = self.connect_db(db_name='postgres', username=self.username, password=self.password)
+        self.conn = self.connect_db(db_name='postgres', username=self.username, password=self.password, url=self.url)
         # verify db exists
         if self.check_db_exists(db_name=self.db_name)[0]['exists']:
             # set connection
-            self.conn = self.connect_db(db_name=self.db_name, username=self.username, password=self.password)
+            self.conn = self.connect_db(db_name=self.db_name, username=self.username, password=self.password,
+                                        url=self.url)
             return self
         else:
             # create db and initialise connection
-            self.create_db(self.db_name, password=self.password, username=self.username)
-            self.conn = self.connect_db(db_name=self.db_name, username=self.password, password=self.password)
-            self.create_tables(conn=self.conn)
+            self.create_db(self.db_name, password=self.password, username=self.username, url=self.url)
+            self.conn = self.connect_db(db_name=self.db_name, username=self.password, password=self.password,
+                                        url=self.url)
+            self.create_tables()
             return self
