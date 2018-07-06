045e2526404c2a0577d984b70537eddc9c69a5a1
fgkinus <fgkinus@gmail.com>
2018-07-06 05:56:30 +0300

    removed unnecessary imports

--- run.py
+++ run.py
@@ -3,8 +3,7 @@ from flask_jwt_extended import JWTManager
 from flask_restplus import Api
 import os
 
-from app.utils import create_app, register_namespace
-from db.utils import Database
+from app import create_app, register_namespace
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)  # create the Flask Instance
@@ -12,22 +11,8 @@ app = create_app(config_name)  # create the Flask Instance
 api = Api(
     app, version='2.0',
     title='RideMyWay API',
-    description="The API to v1-My-Way ride sharing platform.",
+    description="The API to v2-My-Way ride sharing platform.",
 )
-# # database initialisation
-# init_db(db_name=app.config['DATABASE_NAME'], password=app.config['DATABASE_PASSWORD'],
-#         username=app.config['DATABASE_USER'])
-# # connect to database
-# DB = connect_db(db_name=app.config['DATABASE_NAME'], password=app.config['DATABASE_PASSWORD'],
-#                 user=app.config['DATABASE_USER'])
-
-Database = Database(username=app.config['DATABASE_USER'], password=app.config['DATABASE_PASSWORD'],
-                    db_name=app.config['DATABASE_NAME']).init_db()
-
-app.config['DATABASE_CONN'] = Database
-
-# register blueprints
-# register_blueprints(app=app)
 register_namespace(api)
 # configure JWT
 app.config['JWT_SECRET_KEY'] = app.config['SECRET']
