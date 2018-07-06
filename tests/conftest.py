a35f93f193a6fdebec8bf87ea38c4ef3877cd197
fgkinus <fgkinus@gmail.com>
2018-07-06 05:55:27 +0300

    modified imortatio order of packages in pytest
    - This was necessarry to remove the module importation errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,25 @@
 import pytest
 import sys, os
+import run
+from app.utils import Database as db
 from flask_jwt_extended import JWTManager
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+# sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from flask_restplus import Api
 from app import utils
+from db.utils import Database
 
 
 @pytest.fixture
 def test_client():
-    app = utils.create_app('testing')  # create app instance with testing context
+    app = run.create_app('testing')  # create app instance with testing context
     dbname = app.config['DATABASE_NAME']  # get the database name for testing
     username = app.config['DATABASE_USER']
     password = app.config['DATABASE_PASSWORD']
     api = Api(app)  # initialise api instance
-    utils.init_db(db_name=dbname, username=username, password=password)  # initialize db iff missing
-    utils.register_namespace(api)
+    db = Database(db_name=dbname, username=username,
+                  password=password).init_db()  # initialize db iff missing
+    run.register_namespace(api)
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
     # configure JWT
     app.config['JWT_SECRET_KEY'] = app.config['SECRET']
@@ -25,3 +29,12 @@ def test_client():
     ctx.push()
     yield client
     ctx.pop()
+
+
+@pytest.fixture()
+def test_db():
+    dbname = os.getenv('TESTING_DB_NAME')  # get the database name for testing
+    username = os.getenv('TESTING_DB_USER')
+    password = os.getenv('TESTING_DB_PASSWORD')
+    db = Database(username=username, password=password, db_name=dbname)
+    yield db
